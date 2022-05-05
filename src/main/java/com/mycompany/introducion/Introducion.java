386afc0e55fe139c3a7fbf6334c1a23211ea3702
Jean Ramos <jramos@perkel.com>
2022-05-05 12:49:43 -0500

    primer commit

---
+++ src/main/java/com/mycompany/introducion/Introducion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.introducion;
+
+/**
+ *
+ * @author jramos
+ */
+public class Introducion {
+
+    public static void main(String[] args) {
+        
+        String nombre = "Hola mundo";
+        System.out.println("Hello World!");
+        System.out.println(nombre);
+    }
+}
